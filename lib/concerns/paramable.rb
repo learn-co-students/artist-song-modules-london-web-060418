4619e7784fac5aaa72eb4cb5e9fa225a28d10aab
GCdM <gabrielcm.br@gmail.com>
2018-06-12 17:07:48 +0100

    Done.

---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,16 @@
+module Paramable
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
+end
+
+
+
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+  #
+  #
+  #   def to_param
+  #     name.downcase.gsub(' ', '-')
+  #   end
