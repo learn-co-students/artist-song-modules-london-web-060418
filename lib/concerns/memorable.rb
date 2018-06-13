60bc565111913acebdc7de1d2052c211fbd07d68
github username <github email address>
2018-06-13 15:08:59 +0100

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,15 @@
+module Memorable
+
+  @all = []
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+  
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Song
+  extend Memorable
+
   attr_accessor :name
   attr_reader :artist
 
@@ -18,19 +20,20 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
-
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
-
+  # def reset_all
+  #   @@songs.clear
+  # end
+  #
+  # def count
+  #   self.all.count
+  # end
+  #
   def self.all
     @@songs
   end
+
 end
